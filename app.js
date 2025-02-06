cea5eaedf887d280b767fefa8c77a433eb896b56
Caio Alves <caiovictoralvesp@gmail.com>
2025-02-06 19:14:38 -0300

    criada funcao que adiciona os amigos a lista

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo() {
+  let campo = document.querySelector('#amigo');
+  let amigo = campo.value;
+  let listaDeAmigos = document.getElementById('listaAmigos');
+
+  if (!amigo) {
+    alert('Por favor, insira um nome.');
+  } else {
+    amigos.push(amigo);
+    campo.value = '';
+    listaDeAmigos.innerHTML = '';
+
+    amigos.forEach(amigo => {
+      listaDeAmigos.innerHTML += `
+        <li>${amigo}</li>
+      `;
+    });
+  }
+}
